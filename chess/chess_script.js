f8a34552cb05c5cf97fc5de5385f840922e357f2
ShadOnTheWeb <kaden.ashcraft@gmail.com>
2017-07-14 19:08:15 -0600

    chess

--- chess/chess_script.js
+++ chess/chess_script.js
@@ -34,47 +34,54 @@
 //Javascript method:
 
 
-//
-////helper function to fetch the data from an external source
-//function getJSON(url) {
-//    return fetch(url)
-//        .then(function (response) {
-//        return response.json();
-//    })
-//        .catch(function (error) {
-//        console.log(error);
-//    });
-//}
-//
-////lets get some ships
-//function fetchShips() {
-//    var url = "chess_data.json";
-//    //call getJSON function to get the list of ships from the api
-//    getJSON(url).then(function (data) {
-//        //stuff that should happen after the request is done.
-//        console.log(data);
-//        var results = data.results;
-//        var shipListElement = document.getElementById('shiplist');
-//        shipListElement.innerHTML = "";
-//        results.forEach(function(ship) {
-//            var listItem = document.createElement('li');
-//            var anchor = document.createElement('a');
-//            anchor.setAttribute('href',ship.url);
-//            anchor.innerHTML = ship.name;
-//            listItem.appendChild(anchor);
-//            shipListElement.appendChild(listItem);
-//            anchor.addEventListener('click',function(event){
-//                //when clicked the default link behavior should be stopped, and the ship details function should be called, passing the value of the href attritute in. Prevent it from opening the other page and make it add the details to a div on the page instead. How do you stop the default behavior? event.preventDefault(); This replaces the querySelectorAll in part 2.
-//                event.preventDefault();
-//                document.getElementById('model').innerHTML = ship.model;
-//                document.getElementById('cost').innerHTML = ship.cost_in_credits;
-//                document.getElementById('crew').innerHTML = ship.crew;
-//                //getShipDetails(ship.url);
-//            });
-//        });
-//    });
-//}
-//
-//fetchShips();
 
+//helper function to fetch the data from an external source
+function getJSON(url) {
+    return fetch(url)
+        .then(function (response) {
+        return response.json();
+    })
+        .catch(function (error) {
+        console.log(error);
+    });
+}
 
+//lets get some ships
+function fetchShips(chess) {
+    var url = "chess_data.json";
+    //call getJSON function to get the list of ships from the api
+    getJSON(url).then(function (data) {
+        //stuff that should happen after the request is done.
+        console.log(data);
+        var results = data;
+        var shipListElement = document.getElementById("game_info");
+        var selector = document.getElementById("choose_game");
+        var selector_value = document.getElementById("choose_game").innerHTML;
+        var class_of_selector_value = selector.id;
+        var sub_results = results[chess];
+        var elements = shipListElement.children;
+        if (chess=="default") {
+            for (x=0; x<elements.length; x++) {
+                elements[x].innerHTML = "";
+            }
+            document.getElementById("bio_p").innerHTML = "Please choose above which chess game you would like to view...";
+            return;
+        }
+        for (x=0; x<elements.length; x++) {
+            elements[x].innerHTML = sub_results[elements[x].classList[1]];
+        }
+        document.getElementById("bio_p").innerHTML = "Game notation: ";
+        for (a=0; a<sub_results.notation.length; a++) {
+            document.getElementById("bio_p").innerHTML += sub_results.notation[a];
+        }
+        });
+}
+
+function initialize(){
+var selector = document.getElementById("choose_game");
+    selector.addEventListener('change',function(e)     {
+        console.log(e);
+        fetchShips(e.target.value);});
+}
+
+initialize();
